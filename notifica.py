73bdc240b92606374a18e7f4d7ff660d2964020f
Alex Portz <alex.portz@live.com>
2025-01-22 20:03:10 -0300

    Sistema teste de envio Funcionando

--- notifica.py
+++ notifica.py
@@ -1,11 +1,29 @@
 #Importação do cliente Twilio
 from twilio.rest import Client
+from dotenv import load_dotenv
+import os
 
-accID = ''
-tokenID = ''
+load_dotenv()
 
-cliente = Client(accID, tokenID)
+nome="São João Batista"
+myMessage = f'Olá {nome}, Lembramos que o senhor está inadinplente em nosso sitema. https://google.com'
 
-mensagem = cliente.messages.create()
+accID = os.getenv("ACC_SID")
+tokenID = os.getenv("TOKEN_ID")
+senderPhone = os.getenv("PHONE_SENDER")
+receiverPhone = os.getenv("PHONE_RECEIVER")
 
-print(mensagem.body)
+print(accID)
+print(tokenID)
+print(senderPhone)
+print(receiverPhone)
+
+client = Client(accID, tokenID)
+message = client.messages.create(
+  from_= senderPhone,
+  body= myMessage,
+  to= receiverPhone
+)
+
+# print(message.sid)
+print(message.body)
